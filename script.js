f873193a32ff111dc16a829ea17f086e82771828
Marik-D <maretskii@gmail.com>
2019-11-08 18:17:26 +0100

    Remove script

--- script.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const data = require("./data.json");
-
-const newData = {
-  interface: data.interface.map(({ payable, name, stateMutability, ...x }) => x)
-};
-
-fs.writeFileSync("./data1.json", JSON.stringify(newData, null, 4));
